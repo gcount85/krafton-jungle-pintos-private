2c24bd272c31b565a0e5fbcc98de7810ab8a2e48
gcount85 <100mgml@gmail.com>
2022-12-31 16:20:57 +0900

    sema for exec, sema for wait 필드 추가 + 초기화

--- threads/thread.c
+++ threads/thread.c
@@ -219,8 +219,8 @@ tid_t thread_create(const char *name, int priority,
 	t->parent_process = thread_current(); // 부모 가리키는 포인터 추가
 	t->exit_status = 0;
 	t->load_status = 0;
-	// sema_init(&t->sema_for_exec, 0); // `exec()`을 위한 세마포어 초기화
-	// sema_init(&t->sema_for_wait, 0); // `process_wait()`을 위한 세마포어 초기화
+	sema_init(&t->sema_for_exec, 0); // `exec()`을 위한 세마포어 초기화
+	sema_init(&t->sema_for_wait, 0); // `process_wait()`을 위한 세마포어 초기화
 	// list_push_back(&thread_current()->child_list, &t->elem); // 자식 리스트에 추가 (커널 패닉 남, outside elem assert, 오버 플로우 때문?)
 	thread_current()->child_elem = t->elem;
 
